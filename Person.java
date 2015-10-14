6c660729cd9067e2b44967b5ef2c670ffe5a1dec
Roman <nemchinovrp@yandex.ru>
2015-10-14 13:27:12 +0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,37 @@
+package example.using.comparator;
+
+/**
+ *
+ * @author roman
+ */
+public class Person {
+    int id;
+    String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+           return id + " " + name;
+    }
+    
+}
